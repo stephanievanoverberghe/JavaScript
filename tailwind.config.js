f0a5dea8c61b21086ac4821da8c0cc55db7be7eb
Stéphanie Vanoverberghe <orangestreet@live.fr>
2024-04-27 14:05:49 +0200

    fix: update config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/**/*.{html, js, jsx}",
+    "./*.{html, js, jsx}"
+  ],
+  darkMode: 'selector',
+  theme: {
+    // A l'extérieur, ça remplace les fonctionnalités se trouvant dans le thème tailwind
+    extend: {
+      // A l'intérieur, ça ajoute des fonctionnalités au thème tailwind sans rien écraser
+    },
+  },
+  plugins: [],
+}
+
